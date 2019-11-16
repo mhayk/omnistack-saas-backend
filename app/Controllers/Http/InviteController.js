1686e39f9f73bcfe1b80c10502ec2f3219206a23
Mhayk Whandson <hi@mhayk.com>
2019-11-16 10:01:33 +0000

    Invite and Project controller .. initial structure has been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ app/Controllers/Http/InviteController.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with invites
+ */
+class InviteController {
+  /**
+   * Create/save a new invite.
+   * POST invites
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    console.log(request.team)
+  }
+}
+
+module.exports = InviteController
