4829b1c932b031934e04ee297c8aaa4a7ac2db6f
Mhayk Whandson <hi@mhayk.com>
2019-11-17 11:05:00 +0000

    Invitation Hook has been created and registered.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- app/Models/Invite.js
+++ app/Models/Invite.js
@@ -1,9 +1,15 @@
-'use strict';
+'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Invite extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('afterCreate', 'InviteHook.sendInvitationEmail')
+  }
+
   user () {
     return this.belongsTo('App/Models/User')
   }
