e9ee2c5c92bfbaba06f2ce6531dffe02a226a2ec
Mhayk Whandson <hi@mhayk.com>
2019-11-18 21:54:56 +0000

    Project CRUD has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- app/Models/Team.js
+++ app/Models/Team.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
@@ -20,6 +20,10 @@ class Team extends Model {
     return this.belongsToMany('App/Models/User').pivotModel(
       'App/Models/UserTeam'
     )
+  }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
   }
 }
 
